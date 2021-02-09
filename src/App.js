53b328a69ef487cc2062f12e119e01d4d7fcf8e1
CanniJr <cannigialaluw@gmail.com>
2021-02-09 17:59:42 -0500

    no help start over

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import './App.css';
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import {increment, decrement, reset, login, logout} from './actions'
+
 
 function App() {
-  const counter = useSelector(state => state.counter)
-  const logged = useSelector(state => state.isLogged)
-  const dispatch = useDispatch();
-
+ 
   return (
-    <>
-      {logged ?
-        <div className="app">
-          <h1>Hello Redux!</h1>
-          <div className='counter'>
-            <h3>Counter</h3>
-            <p>{counter}</p>
-          </div>
-          <button
-            onClick={() => dispatch(increment())}>+</button>
-          <button
-            onClick={() => dispatch(decrement())}>-</button>
-          <button
-            onClick={() => dispatch(reset())}>Reset</button>
-          <button
-            onClick={() => dispatch(increment(5))}>+ 5</button>
-          <button
-            onClick={() => dispatch(decrement(5))}>- 5</button>
-          
-          <button
-              onClick={() => dispatch(logout())}>Log out</button>
-          <h3>This is the secret!</h3> 
-        </div>
-        :
-        <>
-          <h3>You must be logged in to see this information! </h3>
-          <button
-            onClick={() => dispatch(login())}>Log in</button>
-        </>
-      }
-    </>
+    <h1>Redux Redo</h1>
   );
 }
 
